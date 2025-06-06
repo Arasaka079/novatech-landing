52cc1861f363fa8d6bd6f90870741bbe5f88d97e
Arasaka <bahaeddin365@gmail.com>
2025-06-07 01:54:27 +0330

    feat: add internationalization support with Next.js and Next Intl

---
+++ src/i18n/routing.ts
@@ -0,0 +1,8 @@
+import { defineRouting } from 'next-intl/routing';
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ['en', 'fr'],
+  // The default locale to use when no locale is specified
+  defaultLocale: 'en',
+});
