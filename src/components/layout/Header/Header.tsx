98fc098c392954d02c6331562c11a3ad36b01795
Arasaka <bahaeddin365@gmail.com>
2025-07-06 13:31:16 +0330

    feat: enhance Header component with submenu functionality and styling improvements

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -28,41 +28,46 @@ function Header() {
           setIsSubMenuOpen(null);
         }}></div>
       <nav className="z-40">
-        <div className="absolute max-w-150 mx-auto top-20 left-5 right-5 xl:hidden ">
+        <div className="absolute max-w-150 mx-auto top-20 left-5 right-5 xl:static ">
           <ul
             className={` ${
               isMenuOpen ? 'flex' : 'hidden'
-            } relative flex-col w-full bg-white text-nt-heading font-poppins  shadow-md rounded-md`}>
+            } relative flex-col w-full bg-white text-nt-heading font-poppins font-medium shadow-md rounded-md 
+             xl:flex xl:flex-row xl:shadow-none xl:font-semibold`}>
             <SquareX
               size={36}
               strokeWidth={1.5}
-              className="absolute right-1 top-1 text-nt-accent p-1 rounded-md"
+              className="absolute right-1 top-1 text-nt-accent p-1 rounded-md xl:hidden"
               onClick={() => {
                 setIsMenuOpen(false);
                 setIsSubMenuOpen(null);
               }}
             />
             {menu.map(item => (
-              <li key={item.id} className="px-4 py-3">
-                <button className="flex justify-between items-center w-full" onClick={() => subMenuToggle(item.id)}>
-                  {item.title}
+              <li key={item.id} className={` ${item.submenu ? 'xl:relative group' : ''} px-4 py-3 xl:text-[17px]`}>
+                <button
+                  className={`flex justify-between items-center w-full hover:text-nt-accent transition-all duration-150 cursor-pointer `}
+                  onClick={() => subMenuToggle(item.id)}>
+                  <a href="#">{item.title}</a>
                   {item.submenu && (
                     <ChevronUp
                       size={24}
                       strokeWidth={1.5}
-                      className={`${isSubMenuOpen === item.id ? 'rotate-180' : ''} transition-all duration-200`}
+                      className={`${
+                        isSubMenuOpen === item.id ? 'rotate-180' : ''
+                      } transition-all duration-200 group-hover:rotate-180`}
                     />
                   )}
                 </button>
-                {item.submenu && isSubMenuOpen === item.id && (
-                  <ul className="flex flex-col gap-1 text-sm ml-3 mt-2 p-2 rounded-md border-2 border-indigo-200">
-                    {item.submenu.map(subitem => (
-                      <li key={subitem.id} className="">
-                        {subitem.label}
-                      </li>
-                    ))}
-                  </ul>
-                )}
+                <ul
+                  className={`${
+                    item.submenu && isSubMenuOpen === item.id ? 'flex' : 'hidden'
+                  } flex-col gap-1 text-sm ml-3 mt-2 p-2 rounded-md border-2 border-indigo-200
+                   xl:absolute xl:gap-3 xl:top-full xl:-left-7 xl:min-w-max xl:bg-white xl:shadow-lg xl:m-0 xl:p-4 xl:group-hover:flex`}>
+                  {item.submenu?.map(subitem => (
+                    <li key={subitem.id}>{subitem.label}</li>
+                  ))}
+                </ul>
               </li>
             ))}
           </ul>
